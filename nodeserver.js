5fae5c38b7e4743a1844781e02ef6ef7b78ad912
Villasanti Francisco <villasanti.francisco@gmail.com>
2014-09-02 09:13:39 -0300

    still ordering the content of the project

--- nodeserver.js
+++ nodeserver.js
@@ -41,7 +41,6 @@ function makeThumbnails(){
 
 makeThumbnails();
 app.use("/",express.static(__dirname));
-app.use("/static",express.static(__dirname+"/public"));
 
 app.get("/", function(req,res){
 
